f04a9e83e7a9ee64b347b4e9dcb65e723a2e6b3e
Jevic J <jevic.jardeleza>
2017-09-06 01:19:14 -0500

    Added StrutsLogin to struts-config.xml and added to web.xml

--- StrutsLoginAppl/src/com/StrutsLogin/Action/LoginAction.java
+++ StrutsLoginAppl/src/com/StrutsLogin/Action/LoginAction.java
@@ -3,6 +3,12 @@
  */
 package com.StrutsLogin.Action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionServlet;
 
 /**
@@ -11,5 +17,14 @@ import org.apache.struts.action.ActionServlet;
  */
 public class LoginAction extends ActionServlet {
 
+	public ActionForward execute(ActionMapping mapping, HttpServletRequest request,
+			HttpServletResponse response, ActionForm form)
+	{
+		String forward = "";
+		
+		
+		return mapping.findForward(forward);
+		
+	}
 	
 }
