3443e3d22fcd63c0d16dc5a13395722b60830086
Jevic J <jevic.jardeleza>
2017-09-05 22:32:58 -0500

    Initial Commit for Struts Login Maven Web Application from scratch

---
+++ StrutsLoginAppl/src/com/StrutsLogin/Action/LoginAction.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.StrutsLogin.Action;
+
+import org.apache.struts.action.ActionServlet;
+
+/**
+ * @author Jevic
+ *
+ */
+public class LoginAction extends ActionServlet {
+
+}
